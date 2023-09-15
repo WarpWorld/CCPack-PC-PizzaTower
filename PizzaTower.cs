4eb939c9731d03537c44d06bf38cad1cec5a3d5b
Crowd Control Effect Pack Synchronization Tool <kat@warp.world>
2023-09-15 18:49:39 +0000

    Crowd Control Effect Pack Synchronization Tool - Automated Commit

--- PizzaTower.cs
+++ PizzaTower.cs
@@ -16,7 +16,7 @@ namespace CrowdControl.Games.Packs
     {
         public PizzaTower([NotNull] UserRecord player, [NotNull] Func<CrowdControlBlock, bool> responseHandler,Action<object> statusUpdateHandler) : base(player, responseHandler, statusUpdateHandler)
         {
-            VersionProfiles = new List<VersionProfile> { new VersionProfile("PizzaTower", InitCrashBandicootNSaneTrilogy, DeinitCrashBandicootNSaneTrilogy, null, Direct3DVersion.None), };
+            VersionProfiles = new List<VersionProfile> { new("PizzaTower", InitCrashBandicootNSaneTrilogy, DeinitCrashBandicootNSaneTrilogy, null, Direct3DVersion.None), };
         }
         private AddressChain _chain_thing;
         private void InitCrashBandicootNSaneTrilogy()
@@ -39,41 +39,41 @@ namespace CrowdControl.Games.Packs
                 //new Effect("Peppino Outfit", "outfit", ItemKind.BidWar),
                 //new Effect("Blood Red", "outfit_red", ItemKind.BidWarValue, "outfit"),
                 List<Effect> effects = new List<Effect>{
-                new Effect("Mighty Knight", "t_0"){Duration=10},
-                new Effect("Pizza Face", "_1"){Duration=5},
+                new("Mighty Knight", "t_0"){Duration=10},
+                new("Pizza Face", "_1"){Duration=5},
                 //new Effect("Turn Around", "_2"),
-                new Effect("Boxxed", "t_3"){Duration=10},
-                new Effect("Gustavo Time", "t_4"){Duration=15},
-                new Effect("Wide and Fast", "a_5"){Duration=20},
-                new Effect("Slow and Thin", "a_27"){Duration=20},
-                new Effect("Inverted Move", "_28_hold"){Duration=15},
-                new Effect("GUN", "g_6"){Duration=30},
-                new Effect("Chicken Rain", "t_7"),
-                new Effect("Devil Pizza", "_8"){Duration=20},
+                new("Boxxed", "t_3"){Duration=10},
+                new("Gustavo Time", "t_4"){Duration=15},
+                new("Wide and Fast", "a_5"){Duration=20},
+                new("Slow and Thin", "a_27"){Duration=20},
+                new("Inverted Move", "_28_hold"){Duration=15},
+                new("GUN", "g_6"){Duration=30},
+                new("Chicken Rain", "t_7"),
+                new("Devil Pizza", "_8"){Duration=20},
                 //new Effect("Pillar John", "_9"),
-                new Effect("Speed Up Game", "_10"){Duration=10},
-                new Effect("Hard Mode", "_11"){Duration=30},
-                new Effect("Fire Mouth", "t_12"){Duration=10},
-                new Effect("OHKO", "_13_hold"){Duration=10}, //hold it
-                new Effect("Mirror Mode", "_14_hold"){Duration=15},
-                new Effect("Upside Down Screen", "b_15_hold"){Duration=15},
-                new Effect("Sideways Screen", "b_26_hold"){Duration=15},
-                new Effect("Be Quiet", "_16"),
-                new Effect("Turn on a Dime", "_17_hold"){Duration=10}, // hold it
-                new Effect("Goblin Air Strike", "_18"),
-                new Effect("Secret Time", "s_19"),
-                new Effect("Hide Tiles", "_20"){Duration=15},
-                new Effect("Electric Trail", "_21_hold"){Duration=10}, //hold it
-                new Effect("Add Lap", "_22"),
-                new Effect("Add Lap Deluxe", "_23"),
-                new Effect("Elite Enemies", "_24_hold"){Duration=30},
+                new("Speed Up Game", "_10"){Duration=10},
+                new("Hard Mode", "_11"){Duration=30},
+                new("Fire Mouth", "t_12"){Duration=10},
+                new("OHKO", "_13_hold"){Duration=10}, //hold it
+                new("Mirror Mode", "_14_hold"){Duration=15},
+                new("Upside Down Screen", "b_15_hold"){Duration=15},
+                new("Sideways Screen", "b_26_hold"){Duration=15},
+                new("Be Quiet", "_16"),
+                new("Turn on a Dime", "_17_hold"){Duration=10}, // hold it
+                new("Goblin Air Strike", "_18"),
+                new("Secret Time", "s_19"),
+                new("Hide Tiles", "_20"){Duration=15},
+                new("Electric Trail", "_21_hold"){Duration=10}, //hold it
+                new("Add Lap", "_22"),
+                new("Add Lap Deluxe", "_23"),
+                new("Elite Enemies", "_24_hold"){Duration=30},
                // new Effect("Reset (test)", "_25"),
                         };
                 return effects;
             }
         }
 
-        public override Game Game => new Game(139, "PizzaTower", "PizzaTower", "PC", ConnectorType.PCConnector);
+        public override Game Game => new(139, "PizzaTower", "PizzaTower", "PC", ConnectorType.PCConnector);
         protected override bool IsReady(EffectRequest request)
         {
             string joe = "C:\\Users\\mrbro\\AppData\\Roaming\\PizzaTower_GM2\\ccout.txt";
